8f00a33f030bc1a1c62603eeae6274f2f2fc9743
Jin Qiao <jinqiao@neo.org>
2019-11-22 18:12:29 +0800

    Add two condition test case

---
+++ BooleanCondExample/BooleanCondExample.cs
@@ -0,0 +1,29 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+
+namespace BooleanCondExample
+{
+    public class BooleanCondExample : SmartContract
+    {
+        public static Object Main(string operation, params object[] args)
+        {
+            if (Runtime.Trigger == TriggerType.Verification)
+            {
+                return true;
+            }
+            else if (Runtime.Trigger == TriggerType.Application)
+            {
+                if (operation == "isFinalized")
+                {
+                    return true;
+                }
+                else if (operation == "getOutcome")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ NumericCondExample/NumericCondExample.cs
@@ -0,0 +1,29 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+
+namespace NumericCondExample
+{
+    public class NumericCondExample : SmartContract
+    {
+        public static Object Main(string operation, params object[] args)
+        {
+            if (Runtime.Trigger == TriggerType.Verification)
+            {
+                return true;
+            }
+            else if (Runtime.Trigger == TriggerType.Application)
+            {
+                if (operation == "isFinalized")
+                {
+                    return true;
+                }
+                else if (operation == "getOutcome")
+                {
+                    return 10;
+                }
+            }
+            return false;
+        }
+    }
+}
